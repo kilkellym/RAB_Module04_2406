d86de29a1b6f0148e50c66bc882b4142d369bdb5
kilkellym <michael@archsmarter.com>
2024-08-27 21:55:44 -0400

    Add project files.

---
+++ RAB_Module04_2406/Command2.cs
@@ -0,0 +1,21 @@
+﻿namespace RAB_Module04_2406
+{
+	[Transaction(TransactionMode.Manual)]
+	public class Command2 : IExternalCommand
+	{
+		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+		{
+			// Revit application and document variables
+			UIApplication uiapp = commandData.Application;
+			UIDocument uidoc = uiapp.ActiveUIDocument;
+			Document doc = uidoc.Document;
+
+			// Your code goes here
+			TaskDialog.Show("Test", "This is Command 2");
+
+			return Result.Succeeded;
+		}
+
+	}
+
+}
